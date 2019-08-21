8f803c11cd0ba469f4ca0a190cbac6ff86e86f61
fabcia <45430545+fabcia@users.noreply.github.com>
2019-08-21 12:28:54 +0200

    add sensor type

--- examples/transmitter_simple/transmitter_simple.ino
+++ examples/transmitter_simple/transmitter_simple.ino
@@ -3,14 +3,16 @@
 Sensor433::Transmitter t = Sensor433::Transmitter(5);
 #define SENSORID1 10
 #define SENSORID2 12
+#define TypeSENSORID1 0
+#define TypeSENSORID2 2
 
 void setup() 
 {
 }
 
 void loop() {
-  t.sendWord(SENSORID1,20);
-  t.sendFloat(SENSORID2,22.3);
+  t.sendWord(SENSORID1, TypeSENSORID1,20);
+  t.sendFloat(SENSORID2,TypeSENSORID2, 22.3);
   delay(5000);
 }
 
